2891ce6702964ba3dc8ea8d2befcbc5543fc96f2
Veerendra Singh Kodle <veerendrasingh500@gmail.com>
2021-04-04 09:40:05 +0530

    User Model Created

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+class UserAdminConfig(UserAdmin):
+
+    ordering = ('email', )
+    search_fields = ('email', 'first_name', 'last_name')
+    list_display = ('email', 'first_name', 'last_name', 'is_active')
+    list_filter = ('email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
+
+    fieldsets = (
+        (None, {
+            "fields": (
+                'email', 'first_name', 'last_name'
+            ),
+        }),
+        ('Permission', {
+            'fields':(
+                'is_staff', 'is_active', 'is_superuser'
+            )
+        }
+        ),
+    )
+
+
+    add_fieldsets = (
+        (None, {
+            'classes' : ('wide', ),
+            'fields' : ('email', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
+    
+
+admin.site.register(User, UserAdminConfig)
